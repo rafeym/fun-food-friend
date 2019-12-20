b48d01362fe7de5fd48e487b39c7922cf399f159
rafeym <mohammadrafey0@gmail.com>
2019-12-19 22:20:15 -0500

    DEVELOP: Application can now create and delete items in real-time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import firebase from './firebase'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      currentItem: '',
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    const collection = firebase.database().ref('items')
+    collection.on('value', snapshot => {
+      let items = snapshot.val()
+      let newState = []
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user
+        })
+      }
+      this.setState({
+        items: newState
+      })
+    })
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const collection = firebase.database().ref('items')
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.username
+    }
+    collection.push(item)
+    this.setState({
+      username: '',
+      currentItem: ''
+    })
+  }
+
+  removeItem(itemId){
+    const itemRef = firebase.database().ref(`/items/${itemId}`)
+    itemRef.remove()
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <header>
+          <div className='wrapper'>
+            <h1>Fun Food Friends</h1>
+          </div>
+        </header>
+        <div className='container'>
+          <section className='add-item'>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type='text'
+                name='username'
+                placeholder="What's your name?"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+              <input
+                type='text'
+                name='currentItem'
+                placeholder='What are you bringing?'
+                value={this.state.currentItem}
+                onChange={this.handleChange}
+              />
+              <button>Add Item</button>
+            </form>
+          </section>
+          <section className='display-item'>
+            <div className='wrapper'>
+              <ul>
+                {this.state.items.map(item => {
+                  return(
+                    <li key={item.id}>
+                      <h3>{item.title}</h3>
+                      <p>Brought by: {item.user}</p>
+                      <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </section>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
